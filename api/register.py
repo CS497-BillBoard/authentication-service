67a82c3a8c46fab73c86e0f31d10d1b2e8816441
Shaishav Patel <s434pate@uwaterloo.ca>
2023-02-12 16:09:04 -0500

    minor fix

--- api/register.py
+++ api/register.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 import requests
 import logging
 
+application = Flask(__name__)
+
 @application.route('/signUp', methods = ["GET", "POST"])
 def signUp():
     return
