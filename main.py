3565cbb49a36d506b9e67510432c61ff8c2a1293
Tommy Huang <tommyhuang999@gmail.com>
2023-02-12 16:15:08 -0500

    add flag to require ssl

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import ssl
 def main():
     print("Hello World")
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.verify_mode = ssl.CERT_REQUIRED
     context.load_cert_chain('cert.pem', 'private.pem')
 
 main()
